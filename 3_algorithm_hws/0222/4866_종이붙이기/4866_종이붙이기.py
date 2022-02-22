bc1090d07c9bd9f4a404ec2f0dffb9e850413905
kjasm0714 <kjasm0714@naver.com>
2022-02-22 14:47:11 +0900

    20220222

---
+++ 3_algorithm_hws/0222/4866_종이붙이기/4866_종이붙이기.py
@@ -0,0 +1,21 @@
+
+def paper(n):
+
+    if n >= 2 and memo[n] == 0:
+        if n % 2 == 0:
+            memo[n] = paper(n - 2) * 4 - 1
+
+        if n % 2 == 1:
+            memo[n] = paper(n - 2) * 4 + 1
+    print(memo)
+    return memo[n]
+
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input()) // 10
+    memo = [0] * (n + 1)
+    memo[1] = 1
+    memo[2] = 3
+    # print(memo)
+    print(f'#{tc}',paper(n))
+
--- 3_algorithm_hws/0222/fibo.py
+++ 3_algorithm_hws/0222/fibo.py
@@ -1,12 +1,13 @@
 def fibo(n):
     global cnt
-    print(memo)
+
     cnt += 1
     if n >=2 and memo[n] == 0:
         memo[n] = fibo(n - 1) + fibo(n - 2)
+    print(memo)
     return memo[n]
 
-n = 100
+n = 10
 memo = [0] * (n + 1)
 memo[0] = 0
 memo[1] = 1
