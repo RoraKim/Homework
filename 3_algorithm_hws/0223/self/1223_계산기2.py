35e890d8d5d89828bf26124efb3742bb4cf97b29
kjasm0714 <kjasm0714@naver.com>
2022-02-24 16:22:36 +0900

    20220224

---
+++ 3_algorithm_hws/0223/self/1223_계산기2.py
@@ -0,0 +1,84 @@
+import sys; sys.stdin = open('1223_계산기2.txt')
+class Stack:
+    def __init__(self, size):
+        self.size = size
+        self.arr = [None] * size
+        self.top = -1
+
+    def push(self, n):
+        self.top += 1
+        self.arr[self.top] = n
+
+    def pop(self):
+        if not self.is_empty():
+            result = self.arr[self.top]
+            self.arr[self.top] = None
+            self.top -= 1
+            return result
+
+    def is_empty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+def post_cal(arr):
+    stack = [] * len(arr)
+
+    for i in arr:
+        # 연산자에 해당하면
+        if i in ('+', '-', '/', '*'):
+                b = stack.pop()
+                a = stack.pop()
+                if i == '+':
+                    c = int(a) + int(b)
+                elif i == '-':
+                    c = int(a) - int(b)
+                elif i == '/':
+                    c = int(a) // int(b)
+                elif i == '*':
+                    c = int(a) * int(b)
+                stack.append(c)
+
+        else:
+            stack.append(int(i))
+    return stack
+
+for tc in range(1,11):
+    n = int(input())
+    lst = list(input())
+    stack = Stack(n)
+
+    opr_rank = {'*' : 2, '/' : 2, '-' : 1, '+' : 1}
+
+    post = []
+    for i in lst:
+        if i in opr_rank:
+            #연산자는 스택이 비었으면 push한다
+            if stack.arr[0] == None:
+                stack.push(i)
+
+            else:
+                #i가 연산자인데 스택이 비어있지 않으면서
+                #스택 맨위 연산자의 우선순위가 i보다 같거나 크다면
+                #stack의 연산자를 pop한뒤 출력하고
+                #현재 i는 스택에 push
+                while opr_rank.get(stack.arr[stack.top]) >= opr_rank.get(i):
+                    post.append(stack.pop())
+                    if stack.is_empty():
+                        break
+                stack.push(i)
+        #숫자이면
+        else:
+            post.append(i)
+
+    while not stack.is_empty():
+        post += stack.pop()
+    # print(post)
+
+    print(f'#{tc}',*post_cal(post))
+
+
+
+
+
