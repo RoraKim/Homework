9af1c2e50a6a39d4c84b3018bab790b789518d79
kjasm0714 <kjasm0714@naver.com>
2022-04-01 21:55:53 +0900

    20220401

--- 3_algorithm_hws/0401/p1.py
+++ 3_algorithm_hws/0401/p1.py
@@ -1,12 +1,23 @@
-arr = list(map(int, input().split()))
-n = 7
-print(arr)
+from pprint import pprint
 
-adj = [[] for _ in range(n + 1)]
+def dfs(now):
+    visited[now] = 1
+    print(now, end=' ')
 
-for i in range(0, len(arr)-1, 2):
-    adj[arr[i]].append(arr[i+1])
-    adj[arr[i + 1]].append(arr[i])
-#
+    for next in range(1, v+1):
+        if arr[now][next] == 1 and not visited[next]:
+            dfs(next)
 
-print(adj)
+
+
+v,e = map(int, input().split()) #v는 노드의 갯수, e는 간선의 수
+data = list(map(int, input().split()))
+print(data)
+visited = [0] * (v+1)
+arr = [[0 for _ in range(v+1)]for _ in range(v+1)]
+for i in range(v + 1):
+    arr[data[i * 2]][data[i * 2 + 1]] = 1
+    arr[data[i * 2 + 1]][data[i * 2]] = 1
+
+pprint(arr)
+dfs(1)
